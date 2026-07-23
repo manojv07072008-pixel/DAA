9ef8941741763227b6306c379d8adb262fa7fd87
MANOJ V <manojv07072008@gmail.com>
2026-07-23 21:16:49 +0530

    Delete linear.py

--- linear.py
+++
@@ -1,9 +0,0 @@
-arr = [10,20,30,40,50]
-key = int(input("Enter The Number to search?"))
-for i in range(len(arr)):
-    if arr[i]==key:
-        print("Element fount at index",i)
-        break
-else:
-    print("Element Not Found")
-    
